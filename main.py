f3d0a2e38252d36829719a8af72dc3c547b7a10e
Andrew Ahn <andrewahn21@gmail.com>
2025-03-04 01:19:49 -0800

    built the neural network

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import torch
+from torch import nn
+from model import NeuralNetwork
 
-x = torch.rand(5,3)
+device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
 
-print(x)
+model = NeuralNetwork().to(device)
+print(model)
+
+X = torch.rand(1, 28, 28, device=device)
+logits = model(X)
+prediction_probabilities = nn.Softmax(dim=1)(logits)
+y_prediction = prediction_probabilities.argmax(1)
+print(y_prediction)
